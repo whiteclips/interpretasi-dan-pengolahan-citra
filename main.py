2205aabbfd4d8d9a6370a5ba99eeb04428c0e949
Diki Ardian <ardiandiki369@gmail.com>
2018-09-19 09:20:38 +0700

    fix manifest after migration

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask import Response
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/manifest.json')
+def manifest():
+    data = json.dumps({
+        "short_name": "Image Processing App",
+        "name": "Image Processing App",
+        "icons": [
+            {
+            "src": "/static/image.png",
+            "type": "image/png",
+            "sizes": "256x256"
+            }
+        ],
+        "start_url": "/index.html",
+        "display": "standalone"
+    })
+    return Response(data, mimetype='application/x-web-app-manifest+json')
 
 @app.route('/histogram')
 def histogram():
